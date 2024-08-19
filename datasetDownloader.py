f63c317fabf561a4ca553eba907877a7a26e2610
zeke-code <116798879+DemaControlsUs@users.noreply.github.com>
2024-08-19 11:31:53 +0200

    Improve comments and code

--- datasetDownloader.py
+++ datasetDownloader.py
@@ -1,22 +1,22 @@
-#Lo scopo di questo file è scaricare i file di COCO necessari per la rete tramite FiftyOne
+# Lo scopo di questo file è scaricare il dataset zoo di COCO tramite FiftyOne
 
 import os
 import fiftyone as fo
 import fiftyone.zoo as foz
 
-#Controlla se la cartella è vuota
+# Controlla se la cartella è vuota
 def checkEmptyFolder(path):
     return len( os.listdir(path)) == 0
 
 
 if __name__ == "__main__":
 
-    #Creiamo (se non esiste) la cartella dove andranno le immagini/annotation
-    if os.path.exists("./dataset") == False:
+    # Creiamo (se non esiste) la cartella dove salvare le immagini/annotation
+    if not os.path.exists("./dataset"):
         os.makedirs("dataset")
-        fo.config.dataset_zoo_dir = "./dataset" #Diciamo a FiftyOne dove vogliamo i file
+        fo.config.dataset_zoo_dir = "./dataset" # Configuriamo la cartella di salvataggio del dataset
     
-    #Scarichiamo i vari dataset (train, validation e test) in questo caso COCO 2017
+    # Scarichiamo i vari segmenti del dataset (train, validation e test)
     if checkEmptyFolder("./dataset"):
         dataset = foz.load_zoo_dataset("coco-2017", split="train")
         dataset = foz.load_zoo_dataset("coco-2017", split="validation")
